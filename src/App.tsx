b0e334938e8b581fc5677f03293222630b4093ec
Fahim Ahmed <Fahim1837>
2024-01-08 21:04:37 +0600

    Setting the page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
-  return <div>App</div>;
+  
+	return (
+		<Grid templateColumns='3' >
+			<GridItem colSpan={3}  bg= 'green.200' >menu</GridItem>
+			<Show above="lg">
+			<GridItem colSpan={1} h='100vh' bg= 'green.300' >aside</GridItem>
+			</Show>
+			<GridItem colSpan={{base: 3, lg: 2}} h='100vh' bg= 'green.400' >main</GridItem>
+		</Grid>
+
+		)
 }
 
 export default App;
