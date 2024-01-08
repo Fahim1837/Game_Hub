1dfa8ded0c3799f4a4e00b9eacb96b0a5045a967
Fahim Ahmed <Fahim1837>
2024-01-09 01:35:46 +0600

    Creating Custom-Hook useGame

--- src/components/main-body/GameGrid.tsx
+++ src/components/main-body/GameGrid.tsx
@@ -1,31 +1,7 @@
-import { useEffect, useState } from "react"
-import clientServices from "../../services/client-services"
-
-interface Game {
-    id: number,
-    name: string,
-    slug: string
-}
+import UseGames from "../../hooks/useGame"
 
 function GameGrid () {
-  const [game, setGame] = useState <Game[]> ([])
-  const [error, setError] = useState ('')
-  const [loader, setLoader] = useState (false)
-  useEffect(() => {
-
-    setLoader (true)
-    const req = clientServices.get('/games')
-    
-    req.then ((res) => {
-        setGame(res.data.results)
-        setLoader(false)
-    })
-        .catch ((err) => {
-            setError (err.message)
-            setLoader (false)
-        })
-  }, [])
-
+  const {error, loader, game} = UseGames()
     return (
     <>  
         {error && (<h1>{error}</h1>)}
---
+++ src/hooks/useGame.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import clientServices from "../services/client-services";
+
+interface Game {
+    id: number,
+    name: string,
+    slug: string
+}
+
+function UseGames () {
+const [game, setGame] = useState <Game[]> ([])
+  const [error, setError] = useState ('')
+  const [loader, setLoader] = useState (false)
+  useEffect(() => {
+
+    setLoader (true)
+    const req = clientServices.get('/games')
+    
+    req.then ((res) => {
+        setGame(res.data.results)
+        setLoader(false)
+    })
+        .catch ((err) => {
+            setError (err.message)
+            setLoader (false)
+        })
+  }, [])
+  return {game, error, loader}
+}
+
+export default UseGames
