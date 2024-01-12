6c8b3b733833f12ba3bffdda242648469d9ff543
Fahim Ahmed <Fahim1837>
2024-01-13 02:24:10 +0600

    Project Built

--- src/components/side-bar/GenreList.tsx
+++ src/components/side-bar/GenreList.tsx
@@ -1,7 +1,6 @@
 import { Heading, Stack } from '@chakra-ui/react'
 import useGenres, { Genre } from '../../hooks/useGenres'
 import GenreLine from './GenreLine'
-import CircularLoader from '../loaders/CircularLoader'
 
 interface Props {
     onSelectGenre: (line: Genre) => void
@@ -9,11 +8,10 @@ interface Props {
 }
 
 function GenreList({ selectedGenre, onSelectGenre }: Props) {
-    const { data, error, loader } = useGenres()
+    const { data } = useGenres()
     return (
         <>
-            {error && <h1>{error}</h1>}
-            {loader && <CircularLoader />}
+            
             <Heading
                 px={4}
                 fontSize="3xl"
